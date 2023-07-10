7aa53a208f3377858063423991e826d10f7ce55f
Yu S. Huang <polyactis@gmail.com>
2023-07-10 14:55:44 +0800

    CI_BUILD_REF is deprecated, replaced by CI_COMMIT_SHA. sync changes in testing script.

--- test/test_simulation.py
+++ test/test_simulation.py
@@ -3,18 +3,20 @@ import argparse
 import os
 import shutil
 import sys
+
 from pyflow import WorkflowRunner
 sys.path.insert(0, os.getcwd())
-from software.main import MainFlow
 binary_folder = "software"
 class SampleTestFlow(WorkflowRunner):
-    def __init__(self, simulation_type=None, purity=0.1,
-        segment_stddev_divider=10, call_snps=False, 
+    def __init__(self, simulation_type=None, true_purity=0.1,
+        segment_stddev_divider=10,
+        call_snps=False, window_size=500,
         commit_id=None, debug=0, nCores=4, ref_version=None):
         self.simulation_type = simulation_type
-        self.purity = purity
+        self.true_purity = true_purity
         self.segment_stddev_divider = segment_stddev_divider
         self.call_snps = call_snps
+        self.window_size = window_size
         self.commit_id = commit_id
         self.debug = debug
         self.nCores = nCores
@@ -22,12 +24,17 @@ class SampleTestFlow(WorkflowRunner):
 
         self.simulation_data = "/y/hlab/AccurityTestData/simulation/%s" % \
             self.simulation_type
+        self.true_ploidy_file_path = os.path.join(self.simulation_data, 'ploidy.txt')
+        if not os.path.isfile(self.true_ploidy_file_path):
+            sys.stderr.write("ERROR: True ploidy file %s does not exist! "
+                "Generate it by simulate/calculate_ploidy.pl.")
+            sys.exit(4)
         self.sample_dir = self.simulation_type
         self.result_file = "%s/result_%s_%s_%s.tsv" % (self.sample_dir,
-            self.commit_id, self.simulation_type, self.purity)
+            self.commit_id, self.simulation_type, self.true_purity)
         self.configure_file_path = "configure"
 
-        self.sample_name = "purity.%s"%self.purity
+        self.sample_name = "purity.%s"%self.true_purity
         self.main_output_dir = "%s/%s/infer_result_debug"%(self.sample_dir,
             self.sample_name)
         self.main_output_parent_dir = "%s/%s"%(self.sample_dir, self.sample_name)
@@ -40,7 +47,7 @@ class SampleTestFlow(WorkflowRunner):
         ref_folder_path = "/y/hlab/AccurityTestData/%s" % self.ref_version
         with open(self.configure_file_path,'w') as f:
             f.write("read_length\t101\n")
-            f.write("window_size\t500\n")
+            f.write("window_size\t%s\n"%(self.window_size))
             f.write("reference_folder_path\t%s\n" % ref_folder_path)
             f.write("samtools_path\t/y/program/bin/samtools\n")
             f.write("strelka_path\t/y/program/strelka-2.9.10.centos6_x86_64\n")
@@ -58,7 +65,7 @@ class SampleTestFlow(WorkflowRunner):
         else:
             os.mkdir(self.sample_dir)
         with open(self.result_file,'w') as f:
-            f.write("commit_id\tsample_class\ttruth_purity\ttruth_ploidy\t"
+            f.write("commit_id\tsample_class\ttrue_purity\ttrue_ploidy\t"
                 "Accurity_purity\tAccurity_ploidy\t"
                 "delta_purity\tdelta_ploidy\tcp_recall\tcp_precision\t"
                 "mcp_recall\tmcp_precision\t"
@@ -114,10 +121,12 @@ class SampleTestFlow(WorkflowRunner):
                 "strelka_snp/results/variants/variants.vcf.gz"))
             accurity_starting_step = 2
 
-        ########################
+        ############################
         # STEP 3: Run the main job #
-        ########################
+        ############################
         sys.stderr.write("##### Starting step %s.\n"%(accurity_starting_step))
+        
+        from software.main import MainFlow
         main_flow = MainFlow(self.configure_file_path, tumor_bam, normal_bam,
             output_dir = self.main_output_dir,
             segment_stddev_divider=self.segment_stddev_divider,
@@ -156,20 +165,20 @@ class SampleTestFlow(WorkflowRunner):
             "mcp_precision=$(cat %s/recall_precision.tsv|awk 'NR>1'|cut -f4);" \
             'Accurity_purity=$(sed -n "2p" %s/infer.out.tsv|cut -f1);' \
             'Accurity_ploidy=$(sed -n "2p" %s/infer.out.tsv|cut -f2);' \
-            'truth_purity=%s;truth_ploidy=$(sed -n "2p" %s/ploidy.txt|cut -f3);' \
-            "delta_purity=$(echo ${Accurity_purity} ${truth_purity}|awk '{print $1-$2}');" \
-            "delta_ploidy=$(echo ${Accurity_ploidy} ${truth_ploidy}|awk '{print $1-$2}');" \
+            'true_purity=%s;true_ploidy=$(sed -n "2p" %s|cut -f3);' \
+            "delta_purity=$(echo ${Accurity_purity} ${true_purity}|awk '{print $1-$2}');" \
+            "delta_ploidy=$(echo ${Accurity_ploidy} ${true_ploidy}|awk '{print $1-$2}');" \
             "delta_purity=${delta_purity#-};delta_ploidy=${delta_ploidy#-};" \
             'period=$(sed -n "4p" %s/infer.out.tsv|cut -f2);' \
             'first_peak_int=$(sed -n "4p" %s/infer.out.tsv|cut -f4);' \
             'likelihood=$(sed -n "4p" %s/infer.out.tsv|cut -f1);' \
-            'echo -e "%s\t%s\t${truth_purity}\t${truth_ploidy}\t${Accurity_purity}\t' \
+            'echo -e "%s\t%s\t${true_purity}\t${true_ploidy}\t${Accurity_purity}\t' \
             '${Accurity_ploidy}\t${delta_purity}\t${delta_ploidy}\t${cp_recall}\t'\
             '${cp_precision}\t${mcp_recall}\t${mcp_precision}\t${period}\t' \
             '${first_peak_int}\t${likelihood}" >> %s' % \
             (self.main_output_dir, self.main_output_dir, self.main_output_dir,
             self.main_output_dir, self.main_output_dir, self.main_output_dir,
-            self.purity, self.simulation_data, self.main_output_dir,
+            self.true_purity, self.true_ploidy_file_path, self.main_output_dir,
             self.main_output_dir, self.main_output_dir, self.commit_id,
             self.simulation_type, self.result_file)
         AnalyzeResultJob = self.addTask(
@@ -180,9 +189,10 @@ class SampleTestFlow(WorkflowRunner):
         # STEP 6: Plot copy number and major copy number #
         ##################################################
         cmd = "%s/plotCPandMCP.py -t %s/truth_cnv.tab -i %s/cnv.output.tsv "\
-            "-o %s/plot.cnv.vs.truth.purity%s.png -r %s || exit 2" % \
+            "-o %s/plot.cnv.vs.true.purity%s.png -r %s || exit 2" % \
             (binary_folder, self.simulation_data, self.main_output_dir, 
-            self.main_output_dir, self.purity, os.path.join(ref_folder_path, "genome.dict"))
+            self.main_output_dir, self.true_purity, 
+            os.path.join(ref_folder_path, "genome.dict"))
         PlotCPAndMCPJob = self.addTask(
             ("PlotCPAndMCP.%s"%self.sample_name).replace(".", "_"), cmd,
             dependencies=main_job)
@@ -227,31 +237,36 @@ class SampleTestFlow(WorkflowRunner):
         result_file.close()
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-y', '--simulation_type', required=True,
-        help='which type of simulated sample, i.e. singleclone')
-    parser.add_argument('-p', '--purity', default=0.7,
-        help='which purity for this type of simulated sample')
-    parser.add_argument('-c', '--nCores', type=int, default=8,
-        help="the max number of CPUs to use. Default is 8.")
-    parser.add_argument("-d", "--debug", type=int, default=0,
-        help="Set debug value. Default 0 means no debug info output."
-		"Anything >0 enables more debug output.")
-    parser.add_argument("--segment_stddev_divider", type=float, default=10.0,
-        help="artificially reduce segment noise level")
-    parser.add_argument('--call_snps',  action='store_true', dest='call_snps',
-        help="If toggled, SNPs will be called.")
-    parser.add_argument('-o', '--output_dir', type=str, required=True,
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-y', '--simulation_type', required=True,
+        help="The type of the simulation sample, i.e. singleclone")
+    ap.add_argument('-p', '--true_purity', default=0.7,
+        help="True purity for the simulation sample. "
+        "Default is %(default)s.")
+    ap.add_argument("--segment_stddev_divider", type=float, default=10.0,
+        help="Reduce segment noise level. Default is %(default)s.")
+    ap.add_argument('--call_snps',  action='store_true', dest='call_snps',
+        help="Toggle to call SNPs. Turn it off if a SNP file already exists.")
+    ap.add_argument('-r', '--ref_version', type=str, default="hs37d5",
+        help="The version of referece data used in Accurity. "
+        "Default is %(default)s.")
+    ap.add_argument('-c', '--nCores', type=int, default=8,
+        help="the max number of CPUs to use. Default is %(default)s.")
+    ap.add_argument('-w', '--window_size', type=int, default=500,
+        help="the window size for normalization. Default is %(default)s.")
+    ap.add_argument('-o', '--output_dir', type=str, required=True,
         help="pyflow output directory. Accurity output is in $simulation_type/purity*/")
-    parser.add_argument('-r', '--ref_version', type=str, default="hs37d5",
-        help="The version of referece data used in Accurity")
-    args = parser.parse_args()
+    ap.add_argument("-d", "--debug", type=int, default=0,
+        help="Set debug value. Default 0 means no debug info output."
+            "Anything >0 enables more debug output.")
+    args = ap.parse_args()
     simulation_type = args.simulation_type
     env_dist = os.environ
-    commit_id = env_dist.get('CI_BUILD_REF')[:8]
-    wflow = SampleTestFlow(simulation_type=simulation_type, purity=args.purity,
+    commit_id = env_dist.get('CI_COMMIT_SHA')[:8]
+    wflow = SampleTestFlow(simulation_type=simulation_type, true_purity=args.true_purity,
         segment_stddev_divider=args.segment_stddev_divider,
-        call_snps=args.call_snps, commit_id=commit_id,
+        call_snps=args.call_snps, window_size=args.window_size,
+        commit_id=commit_id,
         debug=args.debug, nCores=args.nCores,
         ref_version=args.ref_version)
     retval = wflow.run(mode="local", nCores=args.nCores,
--- test/test_tcga.py
+++ test/test_tcga.py
@@ -9,140 +9,161 @@ from software.main import MainFlow
 binary_folder = "software"
 
 class TCGATestFlow(WorkflowRunner):
-	def __init__(self, alignment_folder=None, tumor_bam_file_prefix=None, normal_bam_file_prefix=None,
-	             call_snps=False,
-				 output_dir=None, segment_stddev_divider=10, commit_id=None, nCores=4, ref_version=None):
-		self.alignment_folder = alignment_folder
-		self.tumor_bam_file_prefix = tumor_bam_file_prefix
-		self.normal_bam_file_prefix = normal_bam_file_prefix
-		self.call_snps = call_snps
-		self.output_dir = output_dir
-		self.segment_stddev_divider = segment_stddev_divider
-		self.commit_id = commit_id
-		self.nCores = nCores
-		self.ref_version = ref_version
-		self.configure_file_path = "configure"
+    def __init__(self, alignment_folder=None, tumor_bam_file_prefix=None,
+            normal_bam_file_prefix=None,
+            call_snps=False,
+            output_dir=None, segment_stddev_divider=10, commit_id=None,
+            nCores=4, ref_version=None):
+        self.alignment_folder = alignment_folder
+        self.tumor_bam_file_prefix = tumor_bam_file_prefix
+        self.normal_bam_file_prefix = normal_bam_file_prefix
+        self.call_snps = call_snps
+        self.output_dir = output_dir
+        self.segment_stddev_divider = segment_stddev_divider
+        self.commit_id = commit_id
+        self.nCores = nCores
+        self.ref_version = ref_version
+        self.configure_file_path = "configure"
 
-	def workflow(self):
-		############################################
-		# STEP 0: Generate the configure file #
-		############################################
-		sys.stderr.write("Generating the configure file\n")
-		with open(self.configure_file_path,'w') as f:
-			f.write("read_length\t101\n")
-			f.write("window_size\t500\n")
-			f.write("reference_folder_path\t/y/hlab/AccurityTestData/%s\n" % self.ref_version)
-			f.write("samtools_path\t/y/program/bin/samtools\n")
-			f.write("strelka_path\t/y/program/strelka-2.9.10.centos6_x86_64\n")
-			f.write("binary_folder\t%s\n" % binary_folder)
-
-
-		##############################
-		# For each sub sample, do....#
-		##############################
-		tumor_bam_file_source_path = os.path.join(self.alignment_folder, "%s.bam"%self.tumor_bam_file_prefix)
-		tumor_bai_file_source_path = os.path.join(self.alignment_folder, "%s.bam.bai"%self.tumor_bam_file_prefix)
-
-		normal_bam_file_source_path = os.path.join(self.alignment_folder, "%s.bam"%self.normal_bam_file_prefix)
-		normal_bai_file_source_path = os.path.join(self.alignment_folder, "%s.bam.bai"%self.normal_bam_file_prefix)
-
-		if not os.path.isfile(tumor_bam_file_source_path):
-			sys.stderr.write("ERROR: Tumor sample %s does not exist.\n" % tumor_bam_file_source_path)
-			sys.exit(4)
-		if not os.path.isfile(normal_bam_file_source_path):
-			sys.stderr.write("ERROR: Normal sample %s does not exist.\n" % normal_bam_file_source_path)
-			sys.exit(4)
+    def workflow(self):
+        ############################################
+        # STEP 0: Generate the configure file #
+        ############################################
+        sys.stderr.write("Generating the configure file\n")
+        with open(self.configure_file_path,'w') as f:
+            f.write("read_length\t101\n")
+            f.write("window_size\t500\n")
+            f.write("reference_folder_path\t/y/hlab/AccurityTestData/%s\n" % self.ref_version)
+            f.write("samtools_path\t/y/program/bin/samtools\n")
+            f.write("strelka_path\t/y/program/strelka-2.9.10.centos6_x86_64\n")
+            f.write("binary_folder\t%s\n" % binary_folder)
 
 
-		tumor_bam = "tumor.bam"
-		normal_bam = "normal.bam"
+        ##############################
+        # For each sub sample, do....#
+        ##############################
+        tumor_bam_file_source_path = os.path.join(self.alignment_folder, 
+                                        "%s.bam"%self.tumor_bam_file_prefix)
+        tumor_bai_file_source_path = os.path.join(self.alignment_folder, 
+                                        "%s.bam.bai"%self.tumor_bam_file_prefix)
 
-		os.symlink(tumor_bam_file_source_path, tumor_bam)
-		os.symlink(tumor_bai_file_source_path, "tumor.bam.bai")
-		os.symlink(normal_bam_file_source_path, normal_bam)
-		os.symlink(normal_bai_file_source_path, "normal.bam.bai")
+        normal_bam_file_source_path = os.path.join(self.alignment_folder,
+                                        "%s.bam"%self.normal_bam_file_prefix)
+        normal_bai_file_source_path = os.path.join(self.alignment_folder, 
+                                        "%s.bam.bai"%self.normal_bam_file_prefix)
 
-		#default starting from step 1 (call SNPs)
-		main_starting_step = 1
-		snp_vcf_strelka_file_source_path = os.path.join(self.alignment_folder, "%s_strelka_snp.vcf.gz"%self.tumor_bam_file_prefix)
-		if os.path.isfile(snp_vcf_strelka_file_source_path) and not self.call_snps:
-			#create dir to put the symbol link to variants.vcf.gz
-			os.makedirs(os.path.join("strelka_snp/results/variants"))
-			snp_vcf_strelka_file_dst_path = os.path.join("strelka_snp/results/variants/variants.vcf.gz")
-			os.symlink(snp_vcf_strelka_file_source_path, snp_vcf_strelka_file_dst_path)
-			main_starting_step = 2
+        if not os.path.isfile(tumor_bam_file_source_path):
+            sys.stderr.write("ERROR: Tumor sample %s does not exist.\n" % tumor_bam_file_source_path)
+            sys.exit(4)
+        if not os.path.isfile(normal_bam_file_source_path):
+            sys.stderr.write("ERROR: Normal sample %s does not exist.\n" % normal_bam_file_source_path)
+            sys.exit(4)
 
 
-		sys.stderr.write("Working on %s vs %s, output to %s ...\n"%
-		                 (self.tumor_bam_file_prefix,
-		                  self.normal_bam_file_prefix, self.output_dir))
+        tumor_bam = "tumor.bam"
+        normal_bam = "normal.bam"
 
-		########################
-		# STEP 3: Run the main job #
-		########################
-		sys.stderr.write("##### Starting step %s.\n"%main_starting_step)
-		main_flow = MainFlow(self.configure_file_path, tumor_bam, normal_bam,
-		                             output_dir = self.output_dir,
-		                             segment_stddev_divider=self.segment_stddev_divider,
-		                             snp_coverage_min=2,
-		                             snp_coverage_var_vs_mean_ratio=10.0,
-		                             clean=0, step=main_starting_step, debug=1, 
-									 nCores=self.nCores)
-		main_flow.readConfigureFile(self.configure_file_path)
-		main_flow.readDictFile()
-		main_job = self.addWorkflowTask(("Main.%s"%self.tumor_bam_file_prefix).replace(".", "_"),
-		                                   main_flow)
+        os.symlink(tumor_bam_file_source_path, tumor_bam)
+        os.symlink(tumor_bai_file_source_path, "tumor.bam.bai")
+        os.symlink(normal_bam_file_source_path, normal_bam)
+        os.symlink(normal_bai_file_source_path, "normal.bam.bai")
 
-		####################
-		# STEP 7: Clean....#
-		####################
-		sys.stderr.write("Clean ...\n")
-		cmd = "mv %s/*ratio.w*.csv.gz %s/../"%(self.output_dir, self.output_dir)
-		clean_job = self.addTask(("Clean.%s"%self.tumor_bam_file_prefix).replace(".", "_"), cmd, dependencies=main_job)
+        #default starting from step 1 (call SNPs)
+        main_starting_step = 1
+        snp_vcf_strelka_file_source_path = os.path.join(self.alignment_folder, 
+                            "%s_strelka_snp.vcf.gz"%self.tumor_bam_file_prefix)
+        if os.path.isfile(snp_vcf_strelka_file_source_path) and not self.call_snps:
+            #create dir to put the symbol link to variants.vcf.gz
+            os.makedirs(os.path.join("strelka_snp/results/variants"))
+            snp_vcf_strelka_file_dst_path = os.path.join("strelka_snp/results/variants/variants.vcf.gz")
+            os.symlink(snp_vcf_strelka_file_source_path, snp_vcf_strelka_file_dst_path)
+            main_starting_step = 2
 
-	def print_result_to_console(self):
-		"""
-		print
-		    .../infer.out.tsv
+
+        sys.stderr.write("Working on %s vs %s, output to %s ...\n"%
+                         (self.tumor_bam_file_prefix,
+                          self.normal_bam_file_prefix, self.output_dir))
+
+        ########################
+        # STEP 3: Run the main job #
+        ########################
+        sys.stderr.write("##### Starting step %s.\n"%main_starting_step)
+        main_flow = MainFlow(self.configure_file_path, tumor_bam, normal_bam,
+                                     output_dir = self.output_dir,
+                                     segment_stddev_divider=self.segment_stddev_divider,
+                                     snp_coverage_min=2,
+                                     snp_coverage_var_vs_mean_ratio=10.0,
+                                     clean=0, step=main_starting_step, debug=1, 
+                                     nCores=self.nCores)
+        main_flow.readConfigureFile(self.configure_file_path)
+        main_flow.readDictFile()
+        main_job = self.addWorkflowTask(("Main.%s"%self.tumor_bam_file_prefix).replace(".", "_"),
+                                           main_flow)
+
+        ####################
+        # STEP 7: Clean....#
+        ####################
+        sys.stderr.write("Clean ...\n")
+        cmd = "mv %s/*ratio.w*.csv.gz %s/../"%(self.output_dir, self.output_dir)
+        clean_job = self.addTask(("Clean.%s"%self.tumor_bam_file_prefix).replace(".", "_"), 
+                                 cmd, dependencies=main_job)
+
+    def print_result_to_console(self):
+        """
+        print
+            .../infer.out.tsv
             .../infer.out.details.tsv
 
-		:return:
-		"""
-		result_file = open(os.path.join(self.output_dir, "infer.out.tsv"))
-		sys.stderr.write("Content of infer.out.tsv:\n")
-		for line in result_file:
-			sys.stderr.write(line)
-		result_file.close()
+        :return:
+        """
+        result_file = open(os.path.join(self.output_dir, "infer.out.tsv"))
+        sys.stderr.write("Content of infer.out.tsv:\n")
+        for line in result_file:
+            sys.stderr.write(line)
+        result_file.close()
 
-		result_file = open(os.path.join(self.output_dir, "infer.out.details.tsv"))
-		sys.stderr.write("First 15 lines of infer.out.details.tsv:\n")
-		counter = 0
-		for line in result_file:
-			sys.stderr.write(line)
-			counter += 1
-			if counter>=15:
-				break
+        result_file = open(os.path.join(self.output_dir, "infer.out.details.tsv"))
+        sys.stderr.write("First 15 lines of infer.out.details.tsv:\n")
+        counter = 0
+        for line in result_file:
+            sys.stderr.write(line)
+            counter += 1
+            if counter>=15:
+                break
 
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-f', '--alignment_folder', default="/y/Sunset/db/individual_alignment/", type=str, help='individual alignment folder')
-	parser.add_argument('-t', '--tumor_bam_file_prefix', required=True, help='prefix of the tumor bam file basename')
-	parser.add_argument('-n', '--normal_bam_file_prefix', required=True, help='prefix of the normal bam file basename')
-	parser.add_argument('-c', '--nCores', type=int, default=4, help="the max number of CPUs to use.")
-	parser.add_argument('--call_snps',  action='store_true', dest='call_snps', help="If toggled, SNPs will be called.")
-	parser.add_argument('-o', '--output_dir', type=str, required=True, help="output directory to hold the main output")
-	parser.add_argument('-w', '--pyflow_dir', type=str, required=True, help="directory to hold pyflow output")
-	parser.add_argument("--segment_stddev_divider", type=float, default=10.0, help="artificially reduce segment noise level")
-	parser.add_argument('-r', '--ref_version', type=str, default="hs37d5", help="The version of the referece data")
-	args = parser.parse_args()
-	env_dist = os.environ
-	wflow = TCGATestFlow(alignment_folder=args.alignment_folder, tumor_bam_file_prefix=args.tumor_bam_file_prefix,
-	                     normal_bam_file_prefix=args.normal_bam_file_prefix, call_snps=args.call_snps, 
-						 output_dir=args.output_dir,
-	                     segment_stddev_divider=args.segment_stddev_divider,
-	                     commit_id=env_dist.get('CI_BUILD_REF')[:8], nCores=args.nCores, ref_version=args.ref_version)
-	retval = wflow.run(mode="local", nCores=args.nCores, dataDirRoot=args.pyflow_dir, isContinue='Auto',
-	                   isForceContinue=True, retryMax=0)
-	wflow.print_result_to_console()
-	sys.exit(retval)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--alignment_folder', default="/y/Sunset/db/individual_alignment/", 
+                        type=str, help='individual alignment folder')
+    parser.add_argument('-t', '--tumor_bam_file_prefix', required=True, 
+                        help='prefix of the tumor bam file basename')
+    parser.add_argument('-n', '--normal_bam_file_prefix', required=True, 
+                        help='prefix of the normal bam file basename')
+    parser.add_argument('-c', '--nCores', type=int, default=4, 
+                        help="the max number of CPUs to use.")
+    parser.add_argument('--call_snps',  action='store_true', dest='call_snps', 
+                        help="If toggled, SNPs will be called.")
+    parser.add_argument('-o', '--output_dir', type=str, required=True, 
+                        help="output directory to hold the main output")
+    parser.add_argument('-w', '--pyflow_dir', type=str, required=True, 
+                        help="directory to hold pyflow output")
+    parser.add_argument("--segment_stddev_divider", type=float, default=10.0, 
+                        help="artificially reduce segment noise level")
+    parser.add_argument('-r', '--ref_version', type=str, default="hs37d5", 
+                        help="The version of the referece data")
+    args = parser.parse_args()
+    env_dist = os.environ
+    wflow = TCGATestFlow(alignment_folder=args.alignment_folder, 
+                tumor_bam_file_prefix=args.tumor_bam_file_prefix,
+                normal_bam_file_prefix=args.normal_bam_file_prefix, 
+                call_snps=args.call_snps, 
+                output_dir=args.output_dir,
+                segment_stddev_divider=args.segment_stddev_divider,
+                commit_id=env_dist.get('CI_COMMIT_SHA')[:8], 
+                nCores=args.nCores, ref_version=args.ref_version)
+    retval = wflow.run(mode="local", nCores=args.nCores, dataDirRoot=args.pyflow_dir, 
+                       isContinue='Auto',
+                       isForceContinue=True, retryMax=0)
+    wflow.print_result_to_console()
+    sys.exit(retval)
